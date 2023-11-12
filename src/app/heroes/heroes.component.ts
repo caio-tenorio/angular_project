f9a4581dc9cca964dec4aa45e0c3b316fc63ba10
Caio Cavalcanti <cavalcanti971@gmail.com>
2023-11-12 16:50:13 -0300

    herous tour progress

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Hero } from '../hero';
+import { NgFormModule } from '../ngform.module';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, NgFormModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
