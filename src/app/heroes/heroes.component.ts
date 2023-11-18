139fc0d1fa5570354ccd9d40e2edaa0bc71f57c9
Caio Cavalcanti <cavalcanti971@gmail.com~>
2023-11-17 22:49:51 -0300

    tour part four

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 import { NgFormModule } from '../ngform.module';
-import {HEROES} from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, NgFormModule, NgFor],
+  imports: [CommonModule, NgFormModule, NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+  heroes: Hero[] = [];
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
